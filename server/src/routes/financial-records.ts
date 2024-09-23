a81b559ee9f5a4548c873cf8055a1ca1a4161dc4
Ana Clara <ana4ew.br@gmail.com>
2024-09-23 12:21:59 -0300

    Completed: CRUD

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -19,7 +19,7 @@ router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
     }
 });
 
-router.get("/", async (req: Request, res: Response) => {
+router.post("/", async (req: Request, res: Response) => {
     try {
 
         const newRecordBody = req.body;
@@ -32,5 +32,35 @@ router.get("/", async (req: Request, res: Response) => {
         res.status(500).send(err);
     }
 });
+
+router.put("/:id", async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id;
+      const newRecordBody = req.body;
+      const record = await FinancialRecordModel.findByIdAndUpdate(
+        id,
+        newRecordBody,
+        { new: true }
+      );
+  
+      if (!record) return res.status(404).send();
+  
+      res.status(200).send(record);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+      const id = req.params.id;
+      const record = await FinancialRecordModel.findByIdAndDelete(id);
+      if (!record) return res.status(404).send();
+      res.status(200).send(record);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+  
 
 export default router;
